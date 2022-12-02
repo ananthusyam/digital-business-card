b53d2851c86a977f771c5611942469be2df8683a
ananthusyam <109689808+ananthusyam@users.noreply.github.com>
2022-12-02 14:11:44 +0530

    finished the app

---
+++ src/assets/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div className="section">
+            <div className="section-content">
+                <p className="section-header">About</p>
+                <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/assets/components/Interest.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Interest() {
+    return (
+        <div className="section">
+            <div className="section-content">
+                <p className="section-header">Interests</p>
+                <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            </div>
+        </div>
+    )
+}
