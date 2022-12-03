79853ae50096c3683a7dd68b6aa0306b69e12785
ananthusyam <109689808+ananthusyam@users.noreply.github.com>
2022-12-03 13:39:35 +0530

    changes made

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -5,10 +5,10 @@ export default function Footer() {
         <div className="section">
             <div className="footer">
                 <ul className="icon-list">
-                    <li><a href="#" target="_blank"><img src="src/assets/twitter-icon.png" alt="twitter icon" className="icon"/></a></li>
-                    <li><a href="#" target="_blank"><img src="src/assets/facebook-icon.png" alt="facebook icon" className="icon"/></a></li>
-                    <li><a href="#" target="_blank"><img src="src/assets/instagram-icon.png" alt="instagram icon" className="icon"/></a></li>
-                    <li><a href="#" target="_blank"><img src="src/assets/github-icon.png" alt="github icon" className="icon"/></a></li>   
+                    <li className="icon"><a href="#" target="_blank"><img src="src/assets/twitter-icon.png" alt="twitter icon" className="icon"/></a></li>
+                    <li className="icon"><a href="#" target="_blank"><img src="src/assets/facebook-icon.png" alt="facebook icon" className="icon"/></a></li>
+                    <li className="icon"><a href="#" target="_blank"><img src="src/assets/instagram-icon.png" alt="instagram icon" className="icon"/></a></li>
+                    <li className="icon"><a href="#" target="_blank"><img src="src/assets/github-icon.png" alt="github icon" className="icon"/></a></li>   
                 </ul>
             </div>
         </div>
